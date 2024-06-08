6d4c92540452403b19322c0aec3bcff8e27b7260
Samuel <samuelldmj5@gmail.com>
2024-06-08 16:01:17 +0100

    readme added, window.load vs document.ready in jquery

---
+++ readme.md
@@ -0,0 +1,12 @@
+SALIENT POINTS ON JQUERY
+Jquery is a lightweight library that simplifies programming with javascript. 
+=>Compressed version of jquery is suitable for production .ends with min.js
+=>while uncompressed version is suitable for development
+
+
+## $(document).ready vs $(window).load
+=> $(document).ready is a Jquery event that fires as soon as the DOM is loaded and ready to be manipulated by the script.
+*This load first and faster
+
+=> $(window).load it fires when all the contents(images, css, etc) and DOm is fully loaded
+
---
+++ script.js
@@ -0,0 +1,36 @@
+
+//using
+// window.onload = function () {
+//     document.getElementById('button1').addEventListener('click', function () {
+//         alert('Button clicked')
+//     })
+// }
+
+
+//This is deprecated
+// jQuery(document).ready(function () {
+
+// })
+
+//using jquery
+// $(function () {
+//     // DOM Ready - do your stuff
+//     $('#button1').on('click', function () {
+//         alert('Let us go');
+//     })
+
+// });
+
+
+//$(window).load for jquery
+//This not working as expected
+// $(window).load(function () {
+//     alert('Window Loaded')
+// });
+
+// $(document).ready(function () {
+//     alert('dom Loaded')
+// });
+
+
+
